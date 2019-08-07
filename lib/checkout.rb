24bc68f633d70a4cd259305283e6143c725b7fe2
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-07 14:37:54 +0100

    Return 0 when no item is added

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,15 @@
 class Checkout
+
+    def scan(item)
+        @item = item
+    end
+
+    def total
+        total = 0
+    
+        if !@item
+          return total
+        end
+    end
+    
 end
